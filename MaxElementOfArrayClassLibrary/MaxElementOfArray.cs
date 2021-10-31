4721031bc566fb03be67d899593eb48e87536f6f
Bohdan Harabadzhyu <bohdan.harabadzhyu@yandex.ru>
2021-10-31 08:15:50 +0200

    MaxElementOfArray

---
+++ MaxElementOfArrayClassLibrary/MaxElementOfArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Math;
+
+namespace MaxElementOfArrayClassLibrary
+{
+    public class MaxElementOfArray
+    {
+        public int Find(int[] array)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            return RecurseSearch(0, array);
+        }
+
+        private int RecurseSearch(int tmp, int[] array)
+        {
+            return tmp < array.Length - 1 ? Max(array[tmp], RecurseSearch(++tmp, array)) : array[tmp];
+        }
+    }
+}
---
+++ MaxElementOfArrayProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MaxElementOfArrayClassLibrary;
+using static System.Console;
+
+namespace MaxElementOfArrayProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var example1 = new int[] {1, 2, 3, 4, 5,};
+            var example2 = new int[] {1, -10, 3, 4, -4};
+
+            var max = new MaxElementOfArray();
+
+            ShowConsoleResults(max, example1, example2);
+
+            ReadKey();
+        }
+
+        private static void FormatedWriteLine(int[] array, int max)
+        {
+            WriteLine($"Array: {{ {string.Join(", ", array)} }} | Max: {max.ToString()}");
+        }
+
+        private static void ShowConsoleResults(MaxElementOfArray max, params int[][] arrays)
+        {
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                FormatedWriteLine(arrays[i], max.Find(arrays[i]));
+            }
+        }
+    }
+}
